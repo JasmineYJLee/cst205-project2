9d9a0048d14a0afe497610d5d982cd653afaa920
Toutoua Vang <tvang@csumb.edu>
2015-09-30 11:46:49 -0700

    Commit 3: Password Authentication

--- steganography-encode.py
+++ steganography-encode.py
@@ -46,8 +46,21 @@ def encode_message(img, msg):
 #Whatever image we decide to use converted to .bmp
 ori_img = "fish.bmp" 
 img = Image.open(ori_img)
+enc_img = "enc_" + ori_img
 
 #Message to encode into image
 msg = "Something"
 
-encode_message(img,msg)
+img_encoded = encode_message(img,msg)
+if img_encoded:
+     # save the image with the hidden text
+     import os
+     os.startfile(enc_img)
+     print("{} saved!".format(enc_img))
+     password = input('Enter password : ')
+     if(password==1234):
+          #print(password)
+          print("Secret Message : " + msg)  # test
+          img_encoded.save(enc_img)
+          # view the saved file, works with Windows only
+          # behaves like double-clicking on the saved file
