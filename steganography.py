407546bf745fcf730c355ad74e17afcc1b56b1e0
Toutoua Vang <tvang@csumb.edu>
2015-10-12 10:39:35 -0700

    Added decoding message definition instead of just displaying msg variable.

--- steganography.py
+++ steganography.py
@@ -1,13 +1,14 @@
 '''
+STEGANOGRAPHY - Hiding messages in different types of media.
 This part of the program is the encoder.
 It will encode our message into the image.
 '''
 
-#Required module: PIL
+#Required: Libraries
 from PIL import Image
 import getpass
 
-#Definition to encode message into image.
+#Definition to encode message into image
 def encode_message(img, msg):
     #Check message length & mode
     limit = len(msg)
@@ -49,10 +50,32 @@ ori_img = "secret.bmp"
 img = Image.open(ori_img)
 enc_img = "enc_" + ori_img
 
-#Message to encode into image
-msg = getpass.getpass("Set message: ")
+#Definition to decode message from image
+def decode_message(img):
+    width, height = img.size
+    counter = 0
+    msg = ''; #Empty variable to hold message
 
-#Set password
+    for x in range(height):
+        for y in range(width):
+            #Get pixels, for this purpose red is the focus
+            try:
+                r, b, g = img.getpixel((y,x))
+            except:
+                r, b, g, a = img.getpixel((y,x))
+
+            if x == 0 and y == 0:
+                #First red pixel has our message length
+                length = r
+            elif counter <= length:
+                #Change r(ASCII) value to a character and add it to msg
+                msg += chr(r)
+            counter += 1
+    return msg
+
+#Set message to encode into image
+msg = getpass.getpass("Set message: ")
+#Set password to get decoded message
 mypassword = getpass.getpass('Set password: ')
 
 img_encoded = encode_message(img,msg)
@@ -66,11 +89,14 @@ if img_encoded:
     os.startfile(enc_img)
 
     #Password Feature
-    password =  getpass.getpass("Enter password: ")
+    password = getpass.getpass("Enter password: ")
 
     while(password!=mypassword):
         print("Wrong password!\nTry again.")
-        password =  input("Enter password: ")
+        password = getpass.getpass("Enter password: ")
         
     if(password==mypassword):
-        print("Secret Message: " + msg)
+        img2 = Image.open(enc_img)
+        secret_msg = decode_message(img2)
+        print("Secret Message: " + secret_msg)
+        input("Press enter to exit.")
